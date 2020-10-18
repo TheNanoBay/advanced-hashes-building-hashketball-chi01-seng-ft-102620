01ad5e84ffcb95fcdc7fb8f2bb5b6ae1180fac22
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-18 22:23:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,9 +4,20 @@ def game_hash
   
   hasketball_hash = {
     :home = {
-      :team_name = {}, 
-      :colors = {}, 
-      :players = {}
+      :team_name = "", 
+      :colors = [], 
+      :players = [
+        { 
+          :player_name =  " ",
+          :number = " ",
+          :shoe = " ",
+          :points = " ",
+          :rebounds = " ",
+          :assists = " ",
+          :steals = " ",
+          :blocks = " ",
+          :slam_dunks = " "}
+        ]
     },
     :away = {
       :team_name = {}, 
